c246c48283a78dd6f61ebc941e0659477242866d
olga-vorontsovskaya <o.vorontsovskaya@gmail.com>
2024-04-16 15:32:25 +0300

    Add project files.

---
+++ SwitchLearning/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            { 
+               ConsoleKey key = Console.ReadKey().Key;
+               Console.WriteLine();
+               switch (key)
+               {
+                case ConsoleKey.W:
+                    Console.WriteLine("Step forward");
+                    break;
+                    
+                case ConsoleKey.A:
+                    Console.WriteLine("Step to the left");
+                    break;
+                    
+                case ConsoleKey.S:
+                    Console.WriteLine("Step backward");
+                    break;
+                    
+                case ConsoleKey.D:
+                    Console.WriteLine("Step to the right");
+                    break;
+               }
+            }
+        }
+    }
+}
